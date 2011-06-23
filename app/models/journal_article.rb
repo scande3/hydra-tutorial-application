d7a656decb3b0f8f187a4ecdbd139f3494001132
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-06-23 15:26:10 -0500

    Added Hydra::ModelMethods mixin to JournalArticle

--- app/models/journal_article.rb
+++ app/models/journal_article.rb
@@ -1,4 +1,6 @@
 class JournalArticle < ActiveFedora::Base
+  include Hydra::ModelMethods
+  
   has_metadata :name => "descMetadata", :type=> JournalArticleModsDatastream
   has_metadata :name => "rightsMetadata", :type => Hydra::RightsMetadata
 end
--- spec/models/journal_article_spec.rb
+++ spec/models/journal_article_spec.rb
@@ -2,12 +2,23 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe JournalArticle do
   
-  it "should have the specified datastreams" do
-    article = JournalArticle.new
-    article.datastreams.keys.should include("descMetadata")
-    article.datastreams["descMetadata"].class.should == JournalArticleModsDatastream
-    article.datastreams.keys.should include("rightsMetadata")
-    article.datastreams["rightsMetadata"].class.should == Hydra::RightsMetadata
+  before(:each) do
+    # This prevents your tests from hitting Fedora unnecessarily.
+    Fedora::Repository.stubs(:instance).returns(stub_everything())
+    # This gives you a test article object that can be used in any of the tests
+    @article = JournalArticle.new
   end
   
+  it "should have the specified datastreams" do
+    @article.datastreams.keys.should include("descMetadata")
+    @article.datastreams["descMetadata"].class.should == JournalArticleModsDatastream
+    @article.datastreams.keys.should include("rightsMetadata")
+    @article.datastreams["rightsMetadata"].class.should == Hydra::RightsMetadata
+  end
+  
+  it "should have the methods from Hydra::ModelMethods" do
+    JournalArticle.included_modules.should include(Hydra::ModelMethods)
+    @article.should respond_to(:apply_depositor_metadata)
+    @article.should respond_to(:destroyable_child_assets)
+  end
 end
