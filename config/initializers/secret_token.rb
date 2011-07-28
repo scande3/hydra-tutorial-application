c8c2027ea6c58b33a546486a7dbb56db70784acd
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-07-28 17:30:26 -0500

    Initial Commit: empty rails3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HydraTutorialApp::Application.config.secret_token = '21d7f24c153f7fb881cbd4cef2dde757cd5c6a98133a8ab8f6e86776837f1cb835292b57446ab93caf68d592b06748eb703d8c105acaf831d22fa36421054258'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HydraTutorialApp::Application.config.session_store :cookie_store, :key => '_hydra-tutorial-app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HydraTutorialApp::Application.config.session_store :active_record_store
