c1c20b34c898318059474190ec22d7663021a4d1
Matt Zumwalt <matt.zumwalt@yourmediashelf.com>
2011-11-21 14:14:50 -0600

    generated empty rails app with Rails 3.0.10

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HydraTutorialApp::Application.config.secret_token = '14e75b172159752dd52aa62dbce7b04ba2a202397b37812c261f80877093e4c65e211cd26f2a85e9a73701e7fada46a01e3102bd91517c574776af73c1e6bd77'
